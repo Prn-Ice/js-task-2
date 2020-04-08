5799f9fff4038056fcf544551b852b5b81ab1f3e
Prn-Ice <princenna77@gmail.com>
2020-04-08 22:54:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function myFirstFunction(name, age, sex) {
+    // Do stuff
+    console.log(`Hello ${name}`)
+    console.log(age,sex)
+
+    return 'Yay it all works'
+}
+
+
+let myName = 'Prince'
+let x = myFirstFunction(myName, 30, 'Male')
+console.log(x)
