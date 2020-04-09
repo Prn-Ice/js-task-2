03cd88aefba63194e0c6a090baf28bb0b017bf68
Prn-Ice <princenna77@gmail.com>
2020-04-09 12:32:43 +0100

    Update

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
-function myFirstFunction(name, age, sex) {
-    // Do stuff
-    console.log(`Hello ${name}`)
-    console.log(age,sex)
-
-    return 'Yay it all works'
+// Create an object with keys: principal and time
+function createObject(principal, time) {
+  return {
+    principal: principal,
+    time: time,
+  };
 }
 
+// Create an object with keys: principal, rate, time and interest
+function createInterestData(principal, rate, time, interest) {
+  return {
+    principal: principal,
+    rate: rate,
+    time: time,
+    interest: interest,
+  };
+}
 
-let myName = 'Prince'
-let x = myFirstFunction(myName, 30, 'Male')
-console.log(x)
+// Calculate principal using a list of [createObject]
+function principalCalculator(array) {
+  let rate = 1;
+  let interestsData = [];
+
+  array.forEach((element) => {
+    if (element.principal >= 2500) {
+      if (element.time > 1 && element.time < 3) {
+        rate = 3;
+      } else if (element.time >= 3) {
+        rate = 4;
+      }
+    }
+    if (element.principal < 2500 || element.time <= 1) {
+      rate = 2;
+    }
+
+    let interest = (element.principal * rate * element.time) / 100;
+    interestsData.push(
+      createInterestData(element.principal, rate, element.time, interest)
+    );
+  });
+
+  console.log(interestsData);
+  return interestsData;
+}
+
+let data = [
+  createObject(2500, 1.8),
+  createObject(1000, 5),
+  createObject(3000, 1),
+  createObject(2000, 3),
+];
+
+principalCalculator(data);
